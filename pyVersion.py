19374ba9618b37368d462bf8b68f603935c03152
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-08-05 12:12:14 -0400

    Add files via upload

---
+++ pyVersion.py
@@ -0,0 +1,8 @@
+import sys
+#a small project that checks your current version of python installed
+print("Python Version")
+#using the sys attribute that was importedto get the Python Version
+print(sys.version)
+print("Version Information")
+
+print(sys.version_info)
