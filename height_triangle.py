69bb76137dd92069f1d60e96632cd60eefcec81c
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-08-27 11:07:24 -0400

    Add files via upload

---
+++ height_triangle.py
@@ -0,0 +1,11 @@
+# a python exercise program that will compute the base and height of a triangle
+
+#first, let's have the user input the base and height
+
+base = int(input("Enter the base here: "))
+height = int(input("Enter the height here: "))
+
+#create a variable to hold our calculation
+area = base * height / 2
+
+print("The Area = ", area)
