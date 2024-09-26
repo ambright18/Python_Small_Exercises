9a70c53cd08c05091f01ef88e238c66b6c930268
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-09-26 11:24:43 -0400

    Add files via upload

---
+++ ASCII_character.py
@@ -0,0 +1,10 @@
+# a python exercise to get the ASCII value of a character
+
+print()
+#we simply add 'ord' in our print display, encaspulating the character in parentheses and quotation marks.
+print(ord('b'))
+print(ord("B"))
+print(ord('8'))
+print(ord('%'))
+
+print()
