317199390b9342bff4bf50370fde820dc7afda95
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-09-27 11:43:16 -0400

    Add files via upload

---
+++ format_addition.py
@@ -0,0 +1,8 @@
+# a python progam to format two given numbers and display them  using the modulus operator
+
+#first, let's have the user input their desired numbers
+
+num1 = int(input("Enter first number here: "))
+num2 = int(input("Enter second number here: "))
+
+print("\n%d+%d=%d" % (num1, num2, num1+num2))
