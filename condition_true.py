d4426ebd932568452f98cb39a70f07fc138363e4
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-09-30 09:38:19 -0400

    Add files via upload

---
+++ condition_true.py
@@ -0,0 +1,12 @@
+# a python exercise to check if a condition is true.
+# if a number variable is equal to 1, a message should display.
+# if not, then nothing should happen.
+
+number = float(input("Enter a number here: "))
+
+#now to check if the input number is equal to 1
+if (number == 1):
+    print("First month of the year!")
+# if it isn't, print an empty line
+else:
+    print()
