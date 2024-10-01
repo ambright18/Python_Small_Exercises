cfb65d808c35f6dfe9c31dc21ed6c92977a1e679
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-10-01 11:30:56 -0400

    Add files via upload

---
+++ copyright_info.py
@@ -0,0 +1,9 @@
+# a pyton exercise to get the copyright information of Python and write it out
+
+#we need to import the sys module for this one
+import sys
+
+print("\nPython Copyright Information")
+
+print(sys.copyright)
+print()
