5149cfdc722d75c5211975e7965f3529457ef7b5
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-10-02 11:07:12 -0400

    Add files via upload

---
+++ object_type_value.py
@@ -0,0 +1,13 @@
+# a python exercise to get the type and value of an object
+
+#let's define a variable and assign a numeric value to it.
+a = 72
+
+#let's print the identity of the variable
+print("\nIdentity: ", a)
+
+#the data type next
+print("\nType: ", type(a))
+
+#and finally the memory address(value)
+print("\nValue: ", id(a))
